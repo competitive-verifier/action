01f4ab5a59b73343fb06424f70c63e2ff60ee0c6
kzrnm <gengesa@gmail.com>
2022-12-10 19:22:47 +0900

    Add working-directory to oj-resolve

---
+++ oj-resolve/tests-sub/python/hello_world.test.py
@@ -0,0 +1,8 @@
+# verification-helper: PROBLEM https://onlinejudge.u-aizu.ac.jp/courses/lesson/2/ITP1/1/ITP1_1_A
+import hello_world
+
+def main():
+    print(hello_world.get_hello_world())
+
+if __name__ == '__main__':
+    main()
